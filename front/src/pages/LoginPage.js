a426843149b5ec535a8cae5353c50ad13f739332
wafa ben miloud <75492016+wafabenmiloud@users.noreply.github.com>
2023-02-14 05:47:04 +0100

    14 02

--- front/src/pages/LoginPage.js
+++ front/src/pages/LoginPage.js
@@ -11,7 +11,7 @@ export default function LoginPage() {
 
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch("http://localhost:8000/login", {
+    const response = await fetch("https://tense-gold-shift.cyclic.app/login", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
