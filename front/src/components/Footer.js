483575dc53e9bdd8880598684e439c7049c51b9a
wafa ben miloud <wafabenmiloud@gmail.com>
2023-02-22 03:05:15 +0100

    22 02

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -6,10 +6,15 @@ import "./footer.css";
 export default function Footer() {
   return (
     <footer>
-      <Link to="/" className="logo">
-        <img src={logo} alt="logo" />
-      </Link>
-      <span>We'd love to hear from you! Get in touch with us and share your thoughts and opinions on our blog. <br/></span>
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+
+        {" "}
+        <span>
+          We'd love to hear from you! Get in touch with us and share your
+          thoughts and opinions on our blog. <br />
+        </span>
     </footer>
   );
 }
--- front/src/pages/RegisterPage.js
+++ front/src/pages/RegisterPage.js
@@ -3,79 +3,80 @@ import "./RegisterPage.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
+import {BiUser} from 'react-icons/bi';
+import {AiOutlineMail} from 'react-icons/ai';
 
 export default function RegisterPage() {
-	const { getLoggedIn  } = useContext(AuthContext);
+  const { getLoggedIn } = useContext(AuthContext);
   const [data, setData] = useState({
-		firstName: "",
-		lastName: "",
-		email: "",
-		password: "",
-	});
-	const [error, setError] = useState("");
-	const navigate = useNavigate();
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-	const handleChange = ({ currentTarget: input }) => {
-		setData({ ...data, [input.name]: input.value });
-	};
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			const url = "http://localhost:2000/register";
-			await axios.post(url, data);
-			await getLoggedIn();
-			navigate("/");
-		} catch (error) {
-			if (
-				error.response &&
-				error.response.status >= 400 &&
-				error.response.status <= 500
-			) {
-				setError(error.response.data.message);
-			}
-		}
-	};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = "http://localhost:2000/register";
+      await axios.post(url, data);
+      await getLoggedIn();
+      navigate("/");
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
   return (
     <div className="auth">
       <form className="register" onSubmit={handleSubmit}>
         <h3>Get started</h3>
+			<input
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+          onChange={handleChange}
+          value={data.firstName}
+          required
+        />
+        
         <input
-							type="text"
-							placeholder="First Name"
-							name="firstName"
-							onChange={handleChange}
-							value={data.firstName}
-							required
-						/>
-						<input
-							type="text"
-							placeholder="Last Name"
-							name="lastName"
-							onChange={handleChange}
-							value={data.lastName}
-							required
-						/>
-						<input
-							type="email"
-							placeholder="Email"
-							name="email"
-							onChange={handleChange}
-							value={data.email}
-							required
-						/>
-						<input
-							type="password"
-							placeholder="Password"
-							name="password"
-							onChange={handleChange}
-							value={data.password}
-							required
-						/>
-						{error && <div className="error">{error}</div>}
-						<button type="submit">
-							Sing Up
-						</button>
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+          onChange={handleChange}
+          value={data.lastName}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          onChange={handleChange}
+          value={data.email}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={handleChange}
+          value={data.password}
+          required
+        />
+        {error && <div className="error">{error}</div>}
+        <button type="submit">Sing Up</button>
       </form>
     </div>
   );
