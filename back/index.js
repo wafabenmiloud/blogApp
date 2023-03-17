9e61988554499bbfdea093d759314511d42ce943
wafa ben miloud <wafabenmiloud@gmail.com>
2023-03-17 17:51:32 +0100

    17 03

--- back/index.js
+++ back/index.js
@@ -11,6 +11,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 //clien side origin
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(cors({credentials: true, origin: ['http://localhost:3000', 'https://blog-xj3p.onrender.com']}));
 app.set("trust proxy", 1);
 //upload image
